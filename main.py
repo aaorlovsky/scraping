d280e4190d911aa02ee078abceb520a651e9846f
Alexander Orlovsky <aaorlovsky@hotmail.com>
2023-02-16 21:57:04 +0300

    Simple scrapping project

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+from bs4 import BeautifulSoup
+import pprint
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.a.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
+def get_texts_susbtext(res):
+    soup = BeautifulSoup(res.text, 'html.parser')
+    # css selectors . - class, # - id
+    texts = soup.select('.titleline')
+    subtext = soup.select('.subtext')
+    return texts, subtext
+
+
+res = requests.get('https://news.ycombinator.com/news')
+texts, subtext = get_texts_susbtext(res)
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
+texts2, subtext2 = get_texts_susbtext(res2)
+texts = texts + texts2
+subtext = subtext + subtext2
+pprint.pprint(create_custom_hn(texts, subtext))
+
+
